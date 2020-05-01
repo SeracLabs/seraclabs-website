a45247833af112f5cd0e2105f35c82ce5f10599b
mobihack <mobihack.me@gmail.com>
2020-05-01 15:51:10 +0530

    Initial Commit.

--- gulpfile.js
+++ gulpfile.js
@@ -1,38 +1,64 @@
-var gulp        = require('gulp');
+var gulp = require('gulp');
 var browserSync = require('browser-sync').create();
-var sass        = require('gulp-sass');
+var sass = require('gulp-sass');
+var purgecss = require('gulp-purgecss');
+var del = require('del');
 
+gulp.task('moveStatic', function(){
+    return gulp.src('src/static/**/*')
+      .pipe(gulp.dest('dist/static'))
+  });
+
+gulp.task('cleanDist', function(cb) {
+    del.sync('dist');
+    cb();
+  })
+
+  gulp.task('html', function(){
+    return gulp.src('src/**/*.html')
+      .pipe(gulp.dest('dist'))
+  });
 
 // Compile sass into CSS & auto-inject into browsers
-function gulpSass(){
+function gulpSass () {
     return gulp
         .src(['src/sass/*.s?ss'])
         .pipe(sass())
-        .pipe(gulp.dest("src/css"))
+        .pipe(gulp.dest("dist/static/css"))
         .pipe(browserSync.stream());
 }
 
 gulp.task(gulpSass);
 
+// Remove unwanted css
+gulp.task('purgecss', () => {
+    return gulp.src('dist/static/css/**/*.css')
+        .pipe(purgecss({
+            content: ['src/**/*.html']
+        }))
+        .pipe(gulp.dest('dist/static/css'))
+})
+
 // Move the javascript files into our /src/js folder
-function js(){
+function js() {
     return gulp
         .src(['src/js/*.js'])
-        .pipe(gulp.dest("src/js"))
+        .pipe(gulp.dest("dist/js"))
         .pipe(browserSync.stream());
 }
 
 gulp.task(js);
 
 // Static Server + watching scss/html files
-gulp.task('server', gulp.series('gulpSass', function() {
+gulp.task('server', gulp.series('gulpSass', function () {
     browserSync.init({
-        server: "./src"  
+        server: "./dist"
     });
 
     gulp.watch('src/sass/*.s?ss', gulp.series('gulpSass'));
-    gulp.watch("src/*.html").on('change', browserSync.reload);
+    gulp.watch("src/*.html").on('change', gulp.series('html', browserSync.reload));
 }));
 
-gulp.task('serve', gulp.series('gulpSass', 'js', 'server'));
-gulp.task('build', gulp.series('gulpSass', 'js'));
+gulp.task('build', gulp.series('cleanDist', 'moveStatic', 'html', 'gulpSass', 'js'));
+gulp.task('serve', gulp.series('build', 'server'));
+gulp.task('production', gulp.series('build', 'purgecss')); // add minification and more
