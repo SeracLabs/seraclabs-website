417d38a3dcca7cc94805c085631fe8cfc7925ac9
mobihack <mobihack.me@gmail.com>
2020-05-01 17:37:55 +0530

    Linting using semistandard. Ignore `dist/`. More updates.

--- gulpfile.js
+++ gulpfile.js
@@ -4,59 +4,59 @@ var sass = require('gulp-sass');
 var purgecss = require('gulp-purgecss');
 var del = require('del');
 
-gulp.task('moveStatic', function(){
-    return gulp.src('src/static/**/*')
-      .pipe(gulp.dest('dist/static'))
-  });
+gulp.task('moveStatic', function () {
+  return gulp.src('src/static/**/*')
+    .pipe(gulp.dest('dist/static'));
+});
 
-gulp.task('cleanDist', function(cb) {
-    del.sync('dist');
-    cb();
-  })
+gulp.task('cleanDist', function (cb) {
+  del.sync('dist');
+  cb();
+});
 
-  gulp.task('html', function(){
-    return gulp.src('src/**/*.html')
-      .pipe(gulp.dest('dist'))
-  });
+gulp.task('html', function () {
+  return gulp.src('src/**/*.html')
+    .pipe(gulp.dest('dist'));
+});
 
 // Compile sass into CSS & auto-inject into browsers
 function gulpSass () {
-    return gulp
-        .src(['src/sass/*.s?ss'])
-        .pipe(sass())
-        .pipe(gulp.dest("dist/static/css"))
-        .pipe(browserSync.stream());
+  return gulp
+    .src(['src/sass/*.s?ss'])
+    .pipe(sass())
+    .pipe(gulp.dest('dist/static/css'))
+    .pipe(browserSync.stream());
 }
 
 gulp.task(gulpSass);
 
 // Remove unwanted css
 gulp.task('purgecss', () => {
-    return gulp.src('dist/static/css/**/*.css')
-        .pipe(purgecss({
-            content: ['src/**/*.html']
-        }))
-        .pipe(gulp.dest('dist/static/css'))
-})
+  return gulp.src('dist/static/css/**/*.css')
+    .pipe(purgecss({
+      content: ['src/**/*.html']
+    }))
+    .pipe(gulp.dest('dist/static/css'));
+});
 
 // Move the javascript files into our /src/js folder
-function js() {
-    return gulp
-        .src(['src/js/*.js'])
-        .pipe(gulp.dest("dist/js"))
-        .pipe(browserSync.stream());
+function js () {
+  return gulp
+    .src(['src/js/*.js'])
+    .pipe(gulp.dest('dist/js'))
+    .pipe(browserSync.stream());
 }
 
 gulp.task(js);
 
 // Static Server + watching scss/html files
 gulp.task('server', gulp.series('gulpSass', function () {
-    browserSync.init({
-        server: "./dist"
-    });
+  browserSync.init({
+    server: './dist'
+  });
 
-    gulp.watch('src/sass/*.s?ss', gulp.series('gulpSass'));
-    gulp.watch("src/*.html").on('change', gulp.series('html', browserSync.reload));
+  gulp.watch('src/sass/*.s?ss', gulp.series('gulpSass'));
+  gulp.watch('src/*.html').on('change', gulp.series('html', browserSync.reload));
 }));
 
 gulp.task('build', gulp.series('cleanDist', 'moveStatic', 'html', 'gulpSass', 'js'));
